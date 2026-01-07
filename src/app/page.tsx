a063195a2438ad7ad62afd5c18373bcb07aaac80
Akshai Kumar <user.name>
2026-01-07 22:15:48 +0530

    refactor: refactors in the navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,23 @@
-
+import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import Aboutus from "./components/About-us";
+import Statssection from "./components/Statssection";
+import PerformingTeam from "./components/PerformingTeam";
+import Production from "./components/Production";
+import Media from "./components/Media";
+import Footer from "./components/Footer";
 
 function page() {
-  
   return (
     <div className="min-h-screen flex flex-col w-full">
-      <Navbar/>
-      <div className="bg-red-600 size-32 w-full h-screen mx-auto"></div>
-      <div className="bg-blue-600 size-32 w-11/12 h-screen mx-auto"></div>
+      <Navbar />
+      <Hero />
+      <Aboutus />
+      <Statssection />
+      <PerformingTeam/>
+      <Production/>
+      <Media/>
+      <Footer/>
     </div>
   );
 }
