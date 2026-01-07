12d8acf6c6f8f9831b9eedf5a85804b554c3f871
Akshai Kumar <user.name>
2026-01-07 22:12:09 +0530

    page: Hero page for project

---
+++ src/app/components/Hero.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div className="">
+      <div className="bg-red-600 h-184 w-full "></div>
+      <div className="bg-zinc-800/95 w-full h-20 flex items-center justify-center text-white gap-1">
+        Welcome to the
+        <span className="text-yellow-400"> Kalaatatpha - 2025 </span> kannada
+        dance drama production! Join us on 14th & 28th to celebrate!
+        <span className="text-yellow-400">Click here for ticktes</span>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
