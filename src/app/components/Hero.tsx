7cff8b427d1ebd37d645bf836100e82538fd3a0d
Akshai Kumar <user.name>
2026-01-07 23:06:20 +0530

    refactor: adding the carousal for the hero section

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -1,9 +1,57 @@
-import React from "react";
+"use client";
+import React, { useCallback, useEffect, useState } from "react";
+import Heroo from "@/assets/Hero.png";
+import Image from "next/image";
+import Kathak from "@/assets/Kathak.png";
+import Production from "@/assets/Productionimage.png";
+import Yt from "@/assets/YtThumbnail.png";
 
 const Hero = () => {
+  const [active, setActive] = useState(0);
+  const images = [Heroo, Kathak, Production, Yt];
+
+  const nextSlide = useCallback(() => {
+    setActive((prev) => (prev === images.length - 1 ? 0 : prev + 1));
+  }, [images.length]);
+
+  useEffect(() => {
+    const interval = setInterval(nextSlide, 3000);
+    return () => clearInterval(interval);
+  }, [nextSlide]);
+
+  const handleManualChange = (index: number) => {
+    setActive(index);
+  };
   return (
     <div className="">
-      <div className="bg-red-600 h-184 w-full "></div>
+      <div className="relative overflow-hidden w-full">
+        <div
+          className="flex transition-transform duration-500 ease-in-out"
+          style={{ transform: `translateX(-${active * 100}%)` }}
+        >
+          {images.map((img, i) => (
+            <div key={i} className="min-w-full">
+              <Image
+                src={img}
+                alt="Heroo"
+                className="object-fit w-full h-auto"
+              />
+            </div>
+          ))}
+        </div>
+
+        <div className="absolute bottom-4 w-full flex justify-center gap-2">
+          {images.map((_, i) => (
+            <button
+              key={i}
+              onClick={() => handleManualChange(i)}
+              className={`h-3 w-3 rounded-full border border-white transition-all ${
+                active === i ? "bg-white scale-110" : "bg-white/40"
+              }`}
+            />
+          ))}
+        </div>
+      </div>
       <div className="bg-zinc-800/95 w-full h-20 flex items-center justify-center text-white gap-1">
         Welcome to the
         <span className="text-yellow-400"> Kalaatatpha - 2025 </span> kannada
